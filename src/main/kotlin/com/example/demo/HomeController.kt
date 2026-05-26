7079ee08acbfd45ca5cdd36a8294c96c151f7704
HanJeongmok <wjdahe1845@gmail.com>
2026-05-26 14:42:33 +0900

    secret add

--- src/main/kotlin/com/example/demo/HomeController.kt
+++ src/main/kotlin/com/example/demo/HomeController.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
@@ -10,9 +11,12 @@ class HomeController(
     private val s3Client: S3Client
 ) {
 
+    @Value("\${custom.secretWord}")
+    private val secretWord: String = ""
+
     @GetMapping
     fun main(): String {
-        return "Hi"
+        return "Hi, $secretWord"
     }
 
     @GetMapping("/buckets")
